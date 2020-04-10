7acf4882a5afb2b46d77ce21f76c55902f06dbc7
Key <34916202+Lumnca@users.noreply.github.com>
2020-04-10 15:36:33 +0800

    项目初始化
    主页模板完成

---
+++ assets/js/index.js
@@ -0,0 +1,88 @@
+var checkAge = (rule, value, callback) => {
+    if (!value) {
+        return callback(new Error('年龄不能为空'));
+    }
+    setTimeout(() => {
+        if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+        } else {
+            if (value < 18) {
+                callback(new Error('必须年满18岁'));
+            } else {
+                callback();
+            }
+        }
+    }, 1000);
+};
+var validateID = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请输入账号'));
+    } else {
+        if (app.loginForm.id.indexOf("'") != -1 || app.loginForm.id.indexOf("(") != -1 || app.loginForm.id.indexOf(")") != -1) {
+            console.log(app.loginForm.id);
+            callback(new Error('非法字符'));
+        }
+        else {
+            callback();
+        }
+
+    }
+};
+var validatePass = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请输入密码'));
+    } else {
+        callback();
+    }
+};
+var validatePass2 = (rule, value, callback) => {
+    if (value === '') {
+        callback(new Error('请再次输入密码'));
+    } else if (value !== this.loginForm.pass) {
+        callback(new Error('两次输入密码不一致!'));
+    } else {
+        callback();
+    }
+};
+var app = new Vue({
+    el: '#app',
+    data: {
+        search: '',
+        activeName: 'style',
+        ac: 'info',
+        circleUrl: "",
+        islogin: false,
+        centerDialogVisible: false,
+        loginForm: {
+            pass: '',
+            checkPass: '',
+            age: '',
+            id: '',
+            number: 0,
+            result: -1
+        },
+        rules: {
+            pass: [
+                { validator: validatePass, trigger: 'blur' }
+            ],
+            checkPass: [
+                { validator: validatePass2, trigger: 'blur' }
+            ],
+            age: [
+                { validator: checkAge, trigger: 'blur' }
+            ],
+            id: [
+                { validator: validateID, trigger: 'blur' }
+            ]
+        },
+        user: JSON.parse(window.localStorage.getItem('_user')) || { id: '', name: 'Lumnca' },
+        form1: {
+            isCost: '全部',
+            type: []
+        },
+        colors: ['#99A9BF', '#F7BA2A', '#FF9900'],
+        currentPage: 1,
+        pages: 5,
+        isLoginCheck : '未登录'
+    }
+});
