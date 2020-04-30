fbf571a9a3e9cc58639671012ec08f53db65ed79
Key <34916202+Lumnca@users.noreply.github.com>
2020-04-30 12:54:53 +0800

    后台模板搭建

---
+++ admin/js/config.js
@@ -0,0 +1,28 @@
+var eu = [
+    {
+        index : '1',
+        title : '日志',
+        icon: 'el-icon-tickets',
+        menu : [
+            {index : '1-1',title : '登录日志',herf : 'loginlog.html'},
+            {index : '1-2',title : '操作日志',herf : 'loginlog.html'}
+        ]
+    },
+    {
+        index : '2',
+        title : '管理',
+        icon: 'el-icon-menu',
+        menu : [
+            {index : '2-1',title : '用户管理',herf : 'loginlog.html'},
+            {index : '2-2',title : '提交管理',herf : 'loginlog.html'}
+        ]
+    },
+    {
+        index : '3',
+        title : '设置',
+        icon: 'el-icon-setting',
+        menu : [
+            {index : '3-1',title : '网站设置',herf : 'loginlog.html'},
+        ]
+    }
+]
